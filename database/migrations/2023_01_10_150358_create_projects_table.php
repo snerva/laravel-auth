310b044a7dbb63da4d43f164a9acc90983ffd725
snerva <selenenerva@live.it>
2023-01-10 17:51:47 +0100

    seeder and call

--- database/migrations/2023_01_10_150358_create_projects_table.php
+++ database/migrations/2023_01_10_150358_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100)->unique();
+            $table->string('slug');
+            $table->text('description')->nullable;
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +14,13 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->description = $faker->text();
+            $project->save();
+        }
     }
 }
