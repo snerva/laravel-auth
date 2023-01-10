45ea5cddc0962ea0dd34eec6ae9fff67bc208eb4
snerva <selenenerva@live.it>
2023-01-10 18:32:55 +0100

    admin controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +22,10 @@ Route::get('/', function () {
 
 
 Route::middleware('auth')->name('admin')->prefix('admin')->group(function () {
-    Route::get('/', [ProjectController::class, 'index'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('projects', ProjectController::class)->parameters([
+        'projects' => 'project:slug'
+    ]);
 });
 
 require __DIR__ . '/auth.php';
